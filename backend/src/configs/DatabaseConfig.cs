de273c90b6dfb783e7f3d560da829e46dc49e94a
ducewcgke2005 <ducvjpro04@gmail.com>
2026-04-15 01:10:51 +0700

    Database config

---
+++ backend/src/configs/DatabaseConfig.cs
@@ -0,0 +1,8 @@
+namespace CinemaBooking.configs
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
