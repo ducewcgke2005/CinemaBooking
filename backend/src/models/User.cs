d097e0a752acbf91e96036747e6ecb78e2907600
ducewcgke2005 <ducvjpro04@gmail.com>
2026-04-15 01:11:34 +0700

    user model

---
+++ backend/src/models/User.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("email")]
+    public string Email { get; set; }
+
+    [BsonElement("password")]
+    public string Password { get; set; }
+
+    [BsonElement("role")]
+    public string Role { get; set; }
+
+    [BsonElement("userName")]
+    public string UserName { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
